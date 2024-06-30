639f88afaf84dfb703518ccec59a05a14e7faefb
ryanforschool <barretto5736@students.d211.org>
2024-06-30 00:40:27 -0500

    data reading between flask

--- AI4Change/App.js
+++ AI4Change/App.js
@@ -7,10 +7,29 @@ import AboutAIScreen from './screens/AboutAIScreen';
 import AboutPollutionScreen from './screens/AboutPollutionScreen';
 import { Video } from 'expo-av';
 import { View, StyleSheet } from 'react-native';
+import axios from 'axios';
 
 const Stack = createStackNavigator();
 
 export default function App() {
+
+    axios.get('http://10.0.0.156:5000/data')
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+          if (error.response) {
+            // The server responded with a status code outside the 2xx range
+            console.log('Error response:', error.response);
+          } else if (error.request) {
+            // The request was made but no response was received
+            console.log('Error request:', error.request);
+          } else {
+            // Something happened in setting up the request that triggered an error
+            console.log('Error message:', error.message);
+          }
+        });
+
   const videoRef = useRef(null);
 
   return (
