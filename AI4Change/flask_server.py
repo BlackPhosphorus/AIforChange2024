d4b15641de15c98f45f9f8753614cae1fcdc0234
ryanforschool <barretto5736@students.d211.org>
2024-06-30 13:09:47 -0500

    data uupdate

--- AI4Change/flask_server.py
+++ AI4Change/flask_server.py
@@ -9,21 +9,15 @@ CORS(app)
 
 wtr_content_data = ujson.load(open('data/wtr_content.json'))
 air_temps_data = ujson.load(open('data/air_temps.json'))
-wtr_content_data = ujson.load(open('data/wtr_content.json'))
-wtr_content_data = ujson.load(open('data/wtr_content.json'))
-wtr_content_data = ujson.load(open('data/wtr_content.json'))
-wtr_content_data = ujson.load(open('data/wtr_content.json'))
+sea_level_data = ujson.load(open('data/sea_level_pressure.json'))
+surface_pressure_data = ujson.load(open('data/surface_pressure.json'))
+u_wind_data = ujson.load(open('data/u_wind.json'))
+tropopause_data = ujson.load(open('data/tropopause_temp.json'))
 
 
 @app.route('/data', methods=['GET'])
 def get_data():
-    
     return jsonify({"hi" : wtr_content_data["90.0_25.0"][0]})
-
-@app.route('/data', methods=['POST'])
-def post_data():
-    received_data = request.json
-    return jsonify({"received": received_data}), 201
 
 
 if __name__ == "__main__":
