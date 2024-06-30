87fd82f54c67e4b664ba1bbf1698cb09ed053ae9
ryanforschool <barretto5736@students.d211.org>
2024-06-30 11:51:49 -0500

    Update flask_server.py

--- AI4Change/flask_server.py
+++ AI4Change/flask_server.py
@@ -1,14 +1,20 @@
 from flask import Flask, request, jsonify
-from transformers import AutoTokenizer, AutoModel
 import torch
 import json
 from flask_cors import CORS
+import ujson
 
 app = Flask(__name__)
 
 CORS(app)
 
-wtr_content_data = json.load(open('data/wtr_content.json'))
+wtr_content_data = ujson.load(open('data/wtr_content.json'))
+air_temps_data = ujson.load(open('data/wtr_content.json'))
+wtr_content_data = ujson.load(open('data/wtr_content.json'))
+wtr_content_data = ujson.load(open('data/wtr_content.json'))
+wtr_content_data = ujson.load(open('data/wtr_content.json'))
+wtr_content_data = ujson.load(open('data/wtr_content.json'))
+
 
 @app.route('/data', methods=['GET'])
 def get_data():
